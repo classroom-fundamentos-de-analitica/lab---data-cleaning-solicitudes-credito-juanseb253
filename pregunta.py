cec0ee8b313b6616fb1d3566eca3c224f27e95c5
juanseb253 <92771458+juanseb253@users.noreply.github.com>
2023-06-12 16:27:34 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,14 +11,27 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
 
     #
     # Inserte su código aquí
     #
     df.dropna(axis=0, inplace=True)
-    df.sexo = df.sexo.str.lower()
+    df.monto_del_credito = df.monto_del_credito.str.strip('$')
+    df.monto_del_credito = df.monto_del_credito.str.replace(',', '')
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+    df['sexo'] = df['sexo'].str.lower()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.replace('-',' ')
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ')
+    df['línea_credito'] = df['línea_credito'].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.replace('-',' ')
+    df['línea_credito'] = df['línea_credito'].str.replace('_',' ')
+
     return df
 
 if __name__ == "__main__":
-    print(clean_data().sexo.value_counts())
+    print(clean_data())
